b36b3bff5d30d264c3aa703cf180bee9abc5a826
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-09-11 16:34:36 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -12,7 +12,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='ഇപ്പൊ എല്ലാവരെയും അറിയിച്ചേക്കാം...😁...'
+        text='Wait Bro I am Doing You Broadcast You Enjoy The Movies On @CyniteMovies'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
